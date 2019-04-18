bb1396134e7a79e938241644348d6559ad3f947d
George Hategan <george.hategan@geospock.com>
2019-04-18 10:30:24 +0100

    Workaround for SpELAssert validator accepting all nulls as valid

--- src/main/java/cz/jirutka/validator/spring/FieldAssertedRequest.java
+++ src/main/java/cz/jirutka/validator/spring/FieldAssertedRequest.java
@@ -1,7 +1,10 @@
 package cz.jirutka.validator.spring;
 
+import javax.validation.constraints.NotNull;
+
 class FieldAssertedRequest {
 
+  @NotNull
   @SpELAssert("#this != null")
   public String value;
 
